82a7b18b1536d1a2f2c5d43a6e26ed964a1fba2b
Drew Biermann <dbierm03@gmail.com>
2020-05-20 10:53:09 -0600

    Added 2 new scripts

---
+++ autoLogon.ps1
@@ -0,0 +1,10 @@
+﻿Write-Host "This script will bypass windows login screen. Computer will restart after entering password."
+$RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+$DefaultUsername = Read-Host -Prompt "Enter Username" #"your username"
+$DefaultPassword = Read-Host -Prompt "Enter Password" #"your password"
+#$DefaultDomainName = Read-Host -Prompt "Enter Domain"
+Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String 
+Set-ItemProperty $RegPath "DefaultUsername" -Value "$DefaultUsername" -type String 
+Set-ItemProperty $RegPath "DefaultPassword" -Value "$DefaultPassword" -type String
+Set-ItemProperty $RegPath "DefaultDomainName" -Value "$DefaultDomainName" -type String
+Restart-Computer
---
+++ copyLogiShrd.ps1
@@ -0,0 +1,29 @@
+﻿
+#This will prompt for the profile you want to copy the logitech appdata from
+$profile = Read-Host -Prompt "Enter profile to copy Logitech AppData from"
+
+#Deletes logi appdata from all profiles except source. Copies from source to default profile and all other profiles on computer.
+$scriptblock = {
+    $message = "Logitech AppData copied successfully!"
+    try{
+        $source = "C:\Users\$Using:profile\AppData\Roaming\LogiShrd" 
+        $destination = Get-ChildItem 'C:\Users' | Where-Object {$_.Name -ne $Using:profile} | Select-Object -ExpandProperty name
+        Write-Host $destination
+        #remove/copy appdata to default profile for new user
+        Remove-Item -LiteralPath 'c:\users\default\appdata\roaming\logishrd' -Verbose -Recurse -Force
+        Copy-Item -LiteralPath $source -Destination "c:\users\default\appdata\roaming" -Verbose -Recurse -Force -ErrorAction Stop
+        #copy logi appdata to every profile on pc
+        foreach ($user in $destination){
+        $path = "C:\Users\$user\Appdata\Roaming\LogiShrd"
+        Remove-Item -LiteralPath $path -Verbose -Recurse -Force
+        Copy-Item -LiteralPath $source -Destination $path -Verbose -Recurse -Force -ErrorAction Stop
+        }
+    } catch { $message = "Logitech AppData copy failed!"}
+    Write-Host $message
+
+}
+
+#Prompts for computer you wish to run the script on
+$computerName = Read-Host -Prompt "Enter PC Name you wish to run script on"
+
+Invoke-Command -ComputerName $computerName  -ScriptBlock $scriptblock

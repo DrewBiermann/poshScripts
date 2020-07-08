85d22da52b962b546d86476d039f9d26da5b5aa3
Drew Biermann <dbiermann@cityofmontrose.org>
2020-07-08 08:39:46 -0600

    Adding filter function

---
+++ filterOUfunction.ps1
@@ -0,0 +1,11 @@
+﻿function filter-OU{
+    [CmdletBinding()]
+    [Parameter(Mandatory)]
+    param(
+        [string[]]$OU
+        )
+    $filtered = $OU | ForEach-Object {get-adcomputer -Filter "*" -SearchBase $_} | Select -exp Name
+    Write-Output $filtered
+}
+
+#EXAMPLE filter-OU -OU 'ou=Information Systems,dc=mydomain,dc=org','ou=Domain Controllers,dc=mydomain,dc=org'

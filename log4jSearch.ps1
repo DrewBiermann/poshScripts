59ad2a6cefcc092f4b8a22b4d4af9117524f3fbf
Drew Biermann <dbierm03@gmail.com>
2022-03-02 16:25:53 -0700

    Sanitized

--- log4jSearch.ps1
+++ log4jSearch.ps1
@@ -7,13 +7,14 @@ function filter-OU{
     $filtered = $OU | ForEach-Object {get-adcomputer -Filter "*" -SearchBase $_} | Select -exp Name
     Write-Output $filtered
 }
-$computers = "it-testdell","test-surface","is-testvm"
+$computers = filter-OU -OU #filtered domains here
 $onlineComputers = @()
 $fileName = "c:\users\dbiermann\documents\log4jSearch.csv"
 
 $scriptblock = {
+    #write-verbose "Hello" -verbose 4>&1
     New-Object PsObject -Property @{          
-        'Log4J Location' = (Get-PSDrive C -PSProvider FileSystem) | foreach { Get-ChildItem "$($_.Name):\" -Recurse -Include *log4j.jar* -ErrorAction SilentlyContinue}
+        'Log4J Location' = (Get-PSDrive C -PSProvider FileSystem) | foreach { Get-ChildItem "$($_.Name):\" -Recurse -Include *log4j.jar* -ErrorAction SilentlyContinue }
         'PC Name' = hostname
     }
 }
@@ -28,6 +29,12 @@ foreach ($machine in $computers) {
 }
 
 Write-Host "Search for Log4J on $onlineComputers, please be patient..."
-$log4jSearch = Invoke-Command -ComputerName $onlineComputers -ScriptBlock $scriptblock
+try{
+    $log4jSearch = Invoke-Command -ComputerName $onlineComputers -ScriptBlock $scriptblock
+    write-output $log4jSearch
+}catch{
+    Write-Host "Error running invoke-command"
+}
 
-$log4jSearch | Export-Csv $fileName -NoTypeInformation -Append
+
+$log4jSearch | Select-Object 'pc name','log4j location'| Export-Csv $fileName -NoTypeInformation -Append
